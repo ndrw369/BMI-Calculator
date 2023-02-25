7ea6036fc789c8726bd8651b3cac7bf7ff7e7092
indraw <122952802+ndrw369@users.noreply.github.com>
2023-02-25 14:44:11 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function hitungBeratIdeal() {
+  var tinggi = document.getElementById("tinggi").value;
+  var jenisKelamin = document.querySelector(
+    'input[name="jenis-kelamin"]:checked'
+  ).value;
+  var beratIdeal;
+
+  // Menghitung berat ideal berdasarkan rumus IMT (Indeks Massa Tubuh)
+  var imt = ((tinggi / 100) * tinggi) / 100;
+  if (jenisKelamin == "laki-laki") {
+    beratIdeal = imt * 22;
+  } else {
+    beratIdeal = imt * 21;
+  }
+
+  // Menampilkan hasil perhitungan berat ideal
+  document.getElementById("hasil").innerHTML =
+    "Berat ideal Anda adalah " + beratIdeal.toFixed(1) + " kg.";
+}
